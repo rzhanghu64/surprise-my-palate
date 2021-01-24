db7bef25da8520283d019c166f6fb216fe744577
ldominguez721 <Luis.Dominguez42@qmail.cuny.edu>
2021-01-24 13:35:32 -0500

    Fixed up server to obtain JSON in HTTP POST body

--- server/server.js
+++ server/server.js
@@ -7,12 +7,12 @@ const food = require('./food-backend');
 app.use(express.json());
 
 app.post('/foods', (req, res) => {
-  //console.log(req.body)
+  console.log(req.body)
 
   //Server randomly picks three cuisines
-  let f1 = food.pickFood(diet, recent_meals);
+  //let f1 = food.pickFood(diet, recent_meals);
   res.status(200).send();
+
 });
 
-
-
+app.listen(4000);
