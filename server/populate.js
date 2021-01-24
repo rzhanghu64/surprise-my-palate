f193118800c03d394ee375aab6d2bc8289597935
ldominguez721 <Luis.Dominguez42@qmail.cuny.edu>
2021-01-24 03:42:23 -0500

    Let's use lowdb instead, finishing up populate code

--- server/populate.js
+++ server/populate.js
@@ -2,8 +2,27 @@
 
 //Configure dotenv
 require('dotenv').config();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const fs = require('fs');
 
-const levelup = require('levelup');
-const leveldown = require('leveldown');
+const fdb_df = require('./fooddb-default.js');
 
-const db = levelup(leveldown(process.env.DB_FILEPATH))
+//Check if db exists, clear if so
+const clearDB = () => {
+  try {
+    fs.accessSync(process.env.DB_FILEPATH);
+    //Clear it
+    fs.rmSync(process.env.DB_FILEPATH);
+  } catch (err) {
+    //Do nothing
+  }
+}
+clearDB();
+
+//Set up DB
+const adapter = new FileSync(process.env.DB_FILEPATH);
+const db = low(adapter);
+
+//Finally, let's write the defaults
+db.defaults(fdb_df).write();
