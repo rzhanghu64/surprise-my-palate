6f9336e24b118c817cab7a0706beb039622a15f6
ldominguez721 <Luis.Dominguez42@qmail.cuny.edu>
2021-01-24 04:36:28 -0500

    Fixed some things

--- server/food-backend.js
+++ server/food-backend.js
@@ -1,12 +1,31 @@
 'use strict';
 
 const axios = require('axios');
-const 
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const common = require('./common');
+require('dotenv').config()
 
-let f1 = food.pickFood(diet, mealrecent);
+const adapter = new FileSync(process.env.DB_FILEPATH);
+const db = low(adapter);
 
-const letFood = (diet, recent_meals) => {
-  
-}
+/**
+ * Function to pick a random food, constrained by arguments passed.
+ */
+const pickFood = (diet, recent_meals) => {
+  //if()
+};
 
+/**
+ * Function which simply returns a random food from the db.
+ */
+const randomFood = () => {
+  const foods = db.get('foods').value();
+  let randFoodIndex = common.randInt(0, foods.length);
+  return foods[randFoodIndex];
+};
 
+console.log(randomFood());
+console.log(randomFood());
+console.log(randomFood());
+console.log(randomFood());
