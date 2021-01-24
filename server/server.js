e4aefceea9701a6a3a17a2a6f04b545c9d97cd64
rzhanghu64 <46357823+rzhanghu64@users.noreply.github.com>
2021-01-24 13:57:11 -0500

    handlebars engine added to server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 'use strict';
 
+var exphbs = require('express-handlebars');
+var PORT = process.env.PORT || 3000;
+
 const express = require('express');
 const app = express();
 const food = require('./food-backend');
 
 app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, function() {
+  console.log ("server listening on: http://localhost:" + PORT);
+})
 
 app.post('/foods', (req, res) => {
   //console.log(req.body)
