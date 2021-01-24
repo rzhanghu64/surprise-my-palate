5430581493f4b4b1e04fa80ff57c0941aca83163
ldominguez721 <Luis.Dominguez42@qmail.cuny.edu>
2021-01-24 13:13:06 -0500

    Finally, some good functions that should work for obtaining our foods

--- server/test/test.js
+++ server/test/test.js
@@ -4,10 +4,11 @@ const assert = chai.assert;
 const food_backend = require('../food-backend');
 
 describe('server-backend', () => {
+  /*
   it('pickGood', () => {
     let user = {
       'diet': [
-        'gluten'
+        'halal'
       ],
       'meal-history': [
         'chicken',
@@ -19,5 +20,22 @@ describe('server-backend', () => {
     console.log(food_backend.pickFood(user));
     console.log(food_backend.pickFood(user));
 
+  });*/
+  it('getFoods', () => {
+    let nc1 = ['pork'];
+    let nc2 = [];
+    assert.isOk(food_backend.getFoods(nc1));
+    assert.isOk(food_backend.getFoods(nc2));
+    console.log(food_backend.getFoods(nc1));
+    console.log(food_backend.getFoods(nc2));
   });
+
+  it('getSomeFoods', () => {
+    let nc1 = ['pork'];
+    let nc2 = [];
+    assert.isOk(food_backend.getSomeFoods(nc1));
+    assert.isOk(food_backend.getSomeFoods(nc2));
+    console.log(food_backend.getSomeFoods(nc1));
+    console.log(food_backend.getSomeFoods(nc2));
+  })
 });
