41fb7596279b684b1f47f83b415f6fa326bc7828
ldominguez721 <Luis.Dominguez42@qmail.cuny.edu>
2021-01-24 03:49:38 -0500

    Adding default food-db and script to package.json

---
+++ server/fooddb-default.js
@@ -0,0 +1,14 @@
+const fooddb_def = {
+  'foods': {
+    'Nurlan': {
+      'ingredients': ['pork']
+    }
+  },
+  'diets': {
+    'halal': {
+      'non-compatible': ['pork']
+    }
+  }
+};
+
+module.exports = fooddb_def;
